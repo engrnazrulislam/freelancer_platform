b00b3d1567f363f3711e156c02803dc5e59b7c5f
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-09-01 11:01:44 +0600

    user activation part implementing done

--- freelancer_platform/settings.py
+++ freelancer_platform/settings.py
@@ -137,8 +137,8 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL':True,
     'SERIALIZERS': {
         'user_create': 'users.serializers.UserCreateSerializer',
