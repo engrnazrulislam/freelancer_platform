aa2ddf9c5f5359bf28acd5ba7ec1102c8aeae7e4
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-09-05 01:15:52 +0600

    settings supabase

--- freelancer_platform/urls.py
+++ freelancer_platform/urls.py
@@ -9,7 +9,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="PhiMart Rest API",
+      title="Freelancer Platform",
       default_version='v1',
       description="PhiMart Rest API Documentation",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/v1/',include('api.urls')),
